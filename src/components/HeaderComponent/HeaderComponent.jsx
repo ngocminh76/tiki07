e853f0b26a4d04fe0870fa5dcc780fbbbf26cbae
ngocminh76 <ngocminhqnbk@gmail.com>
2023-07-27 14:53:31 +0700

    bài số 3

--- src/components/HeaderComponent/HeaderComponent.jsx
+++ src/components/HeaderComponent/HeaderComponent.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
-
+import { Col } from "antd";
+import React from "react";
+import {
+  WrapperHeader,
+  WrapperHeaderAccount,
+  WrapperTextHeader,
+  WrapperTextHeaderSmall,
+} from "./style";
+import Search from "antd/es/input/Search";
+import {
+  UserOutlined,
+  CaretDownOutlined,
+  ShoppingCartOutlined,
+} from "@ant-design/icons";
 const HeaderComponent = () => {
   return (
-    <div>HeaderComponent</div>
-  )
-}
+    <div>
+      <WrapperHeader gutter={16}>
+        <Col span={6} >
+          <WrapperTextHeader>LẬP TRÌNH THẬT DỄ</WrapperTextHeader>
+        </Col>
+        <Col span={12}>
+          <Search placeholder="input search text" />
+        </Col>
+        <Col span={6} style={{display : 'flex', gap:'20px'}}>
+          <WrapperHeaderAccount>
+            <UserOutlined style={{ fontSize: "30px" }} />
+            <div>
+              <WrapperTextHeaderSmall>
+                Đăng nhập/ Đăng kí
+              </WrapperTextHeaderSmall>
+              <div>
+                <WrapperTextHeaderSmall>Tài khoản</WrapperTextHeaderSmall>
+                <CaretDownOutlined />
+              </div>
+            </div>
+          </WrapperHeaderAccount>
+          <div>
+            <div>
+              <ShoppingCartOutlined style={{fontSize: '30px' , color:'#fff'}} />
+              <WrapperTextHeaderSmall>Giỏ hàng</WrapperTextHeaderSmall>
+            </div>
+          </div>
+        </Col>
+      </WrapperHeader>
+    </div>
+  );
+};
 
-export default HeaderComponent
+export default HeaderComponent;
